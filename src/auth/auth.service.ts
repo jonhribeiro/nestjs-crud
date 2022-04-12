d92495d8cc3344e7716390d2c6915b14615f1b91
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2022-04-11 21:16:19 -0300

    feat: login usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserToken } from './models/UserToken';
+import { JwtService } from '@nestjs/jwt';
+import { USerPayload } from './models/UserPayload';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
-
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+    
+    login(user: User): UserToken {
+        const payload: USerPayload = {
+            sub: user.id,
+            email: user.email,
+            nome: user.nome
+        }
+        const jwtToken = this.jwtService.sign(payload)
+        return {
+            access_token: jwtToken
+        }
+    }
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email)
-        return user
+        
         if (user) {
-            const isPasswordValid = bcrypt.compare(password, user.password);
+            const isPasswordValid = await bcrypt.compare(password, user.password);
             if (isPasswordValid) {
                 return {
                     ...user,
                     password: undefined
-                }
+                };
             }
         }
         throw new Error('Email ou password incorreto')
---
+++ src/auth/models/UserPayload.ts
@@ -0,0 +1,7 @@
+export interface USerPayload {
+    sub: number
+    email: string
+    nome: string
+    iat?: number
+    exp?: number
+}
